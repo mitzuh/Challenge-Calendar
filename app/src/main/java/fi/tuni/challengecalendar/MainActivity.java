aa046aa0b24597d6718616d6d8b8eb126ef16494
Jimi Savola <savola.jimi@gmail.com>
2019-03-11 18:32:44 +0200

    Add date to display in TextView
    Currently selected date displays on top of the calendar in TextView

--- app/src/main/java/fi/tuni/challengecalendar/MainActivity.java
+++ app/src/main/java/fi/tuni/challengecalendar/MainActivity.java
@@ -1,7 +1,14 @@
 package fi.tuni.challengecalendar;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = findViewById(R.id.dateText);
+        final CalendarView calendarView = findViewById(R.id.calendarView);
+
+        final SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+        String selectedDate = sdf.format(new Date(calendarView.getDate()));
+        textView.setText(selectedDate);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String d = String.format("%02d", dayOfMonth);
+                String m = String.format("%02d", month+1);
+                textView.setText(d + "." + m + "." + year);
+            }
+        });
     }
 }
