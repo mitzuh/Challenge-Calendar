c999fb39ee29b8aaa2ccbe3191228fb94b9bc09d
Jimi Savola <savola.jimi@gmail.com>
2019-04-07 18:07:59 +0300

    Delete unused imports

--- app/src/main/java/fi/tuni/challengecalendar/FailedViewActivity.java
+++ app/src/main/java/fi/tuni/challengecalendar/FailedViewActivity.java
@@ -3,10 +3,6 @@ package fi.tuni.challengecalendar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
