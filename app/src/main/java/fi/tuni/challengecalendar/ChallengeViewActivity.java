3c0b6a71457592e59f574686978a2900c62e99f7
Jimi Savola <savola.jimi@gmail.com>
2019-04-07 17:39:22 +0300

    Add ArrayAdapter for Challenges
    - Add own ArrayAdapter for challenges, so that ListView can show the name of the challenge and it's deadline in different columns

--- app/src/main/java/fi/tuni/challengecalendar/ChallengeViewActivity.java
+++ app/src/main/java/fi/tuni/challengecalendar/ChallengeViewActivity.java
@@ -5,8 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class ChallengeViewActivity extends AppCompatActivity {
@@ -15,19 +16,18 @@ public class ChallengeViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenge_view);
 
+        ListView listView = (ListView) findViewById(R.id.listView);
+
         DatabaseHandler db = new DatabaseHandler(this);
         List<Challenge> list = db.getChallenges();
 
-        final Challenge [] challenges = new Challenge[list.size()];
-
-        for (int i=0; i< list.size(); i++) {
-            challenges[i] = new Challenge(list.get(i).id, list.get(i).name, list.get(i).date);
+        ArrayList<Challenge> challenges = new ArrayList<>();
+        for (int i=0; i<list.size(); i++) {
+            challenges.add(list.get(i));
         }
 
-        ArrayAdapter<Challenge> adapter = new ArrayAdapter<Challenge>(this,
-                                            android.R.layout.simple_list_item_1, challenges);
-
-        ListView listView = findViewById(R.id.listView);
+        ChallengeListAdapter adapter =
+                new ChallengeListAdapter(this, R.layout.adapter_view_layout, challenges);
         listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -36,7 +36,7 @@ public class ChallengeViewActivity extends AppCompatActivity {
                 Intent i = new Intent(getApplicationContext(), EditChallengeActivity.class);
                 Bundle b = new Bundle();
 
-                b.putParcelable("challenge", challenges[position]);
+                b.putParcelable("challenge", challenges.get(position));
                 i.putExtras(b);
 
                 startActivity(i);
